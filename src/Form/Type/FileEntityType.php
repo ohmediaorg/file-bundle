9e8b61ed8114486693d9d1f9a2014166bc1561e7
Justin Hoffman <justin@ohmedia.ca>
2023-05-23 16:16:10 -0600

    customize the file widget so a file can be removed without uploading a new one

--- src/Form/Type/FileEntityType.php
+++ src/Form/Type/FileEntityType.php
@@ -5,13 +5,20 @@ namespace OHMedia\FileBundle\Form\Type;
 use OHMedia\FileBundle\Entity\File;
 use OHMedia\FileBundle\Service\FileManager;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FileEntityType extends AbstractType
 {
+    const ACTION_KEEP = 'keep';
+    const ACTION_REPLACE = 'replace';
+    const ACTION_DELETE = 'delete';
+
     private $manager;
 
     public function __construct(FileManager $manager)
@@ -23,23 +30,43 @@ class FileEntityType extends AbstractType
     {
         $file = isset($options['data']) ? $options['data'] : null;
 
-        $help = 'The current file is <a target="_blank" href="%s">%s</a>. Upload a new file to replace it.';
-
         $builder
             ->add('file', FileType::class, [
                 'label' => $options['file_label'],
                 'required' => false,
-                'help' => $file && $file->getPath()
-                    ? sprintf(
-                        $help,
-                        $this->manager->getWebPath($file),
-                        $file->getFilename()
-                    )
-                    : '',
-                'help_html' => true,
                 'constraints' => $options['file_constraints']
             ])
         ;
+
+        if ($file && $file->getPath()) {
+            $keepLabel = sprintf(
+                'Keep the current file <a target="_blank" href="%s">%s</a>',
+                $this->manager->getWebPath($file),
+                $file->getFilename()
+            );
+
+            $builder->add('action', ChoiceType::class, [
+                'mapped' => false,
+                'expanded' => true,
+                'data' => self::ACTION_KEEP,
+                'label' => false,
+                'label_html' => true,
+                'choices' => [
+                    $keepLabel => self::ACTION_KEEP,
+                    'Upload a new file' => self::ACTION_REPLACE,
+                    'Delete after submit' => self::ACTION_DELETE,
+                ],
+            ]);
+        }
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $file = isset($options['data']) ? $options['data'] : null;
+
+        $view->vars['current_file'] = $file && $file->getPath() ? $file : null;
+
+        $view->vars['action_replace'] = self::ACTION_REPLACE;
     }
 
     public function configureOptions(OptionsResolver $resolver)
