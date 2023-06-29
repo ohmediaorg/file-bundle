486a97f88f78cca684ab6fa6a1e80309d20f5a2d
Justin Hoffman <justin@ohmedia.ca>
2023-06-29 16:39:41 -0600

    make sure any malicious files uploaded are displayed as plain text

--- src/Service/FileManager.php
+++ src/Service/FileManager.php
@@ -59,8 +59,20 @@ class FileManager
             return null;
         }
 
+        $mimeType = $file->getMimeType();
+
+        $maliciousMimeTypes = [
+            'application/x-httpd-php',
+            'application/xhtml+xml',
+            'text/html',
+        ];
+
+        if (in_array($mimeType, $maliciousMimeTypes)) {
+            $mimeType = 'text/plain';
+        }
+
         $response = new BinaryFileResponse($physicalFile);
-        $response->headers->set('Content-Type', $file->getMimeType());
+        $response->headers->set('Content-Type', $mimeType);
 
         BinaryFileResponse::trustXSendfileTypeHeader();
 
