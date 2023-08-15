ef274223b50d04bd7606bdab91ea7a3c044442b6
Justin Hoffman <justin@ohmedia.ca>
2023-08-15 11:42:55 -0600

    voter stuff

--- src/Security/Voter/FileFolderVoter.php
+++ src/Security/Voter/FileFolderVoter.php
@@ -2,15 +2,13 @@
 
 namespace OHMedia\FileBundle\Security\Voter;
 
-use OHMedia\FileBundle\Entity\File;
+use OHMedia\FileBundle\Entity\FileFolder;
 use OHMedia\SecurityBundle\Entity\User;
 use OHMedia\SecurityBundle\Security\Voter\AbstractEntityVoter;
 
-class FileVoter extends AbstractEntityVoter
+class FileFolderVoter extends AbstractEntityVoter
 {
-    public const INDEX = 'index';
     public const CREATE = 'create';
-    // NOTE: this is only for frontend; there is no backend file view
     public const VIEW = 'view';
     public const EDIT = 'edit';
     public const MOVE = 'move';
@@ -19,7 +17,6 @@ class FileVoter extends AbstractEntityVoter
     protected function getAttributes(): array
     {
         return [
-            self::INDEX,
             self::CREATE,
             self::VIEW,
             self::EDIT,
@@ -30,35 +27,30 @@ class FileVoter extends AbstractEntityVoter
 
     protected function getEntityClass(): string
     {
-        return File::class;
+        return FileFolder::class;
     }
 
-    protected function canIndex(File $file, User $loggedIn): bool
+    protected function canCreate(FileFolder $file, User $loggedIn): bool
     {
         return $file->isBrowser();
     }
 
-    protected function canCreate(File $file, User $loggedIn): bool
+    protected function canView(FileFolder $file, User $loggedIn): bool
     {
         return $file->isBrowser();
     }
 
-    protected function canView(File $file, User $loggedIn): bool
-    {
-        return true;
-    }
-
-    protected function canEdit(File $file, User $loggedIn): bool
+    protected function canEdit(FileFolder $file, User $loggedIn): bool
     {
         return $file->isBrowser();
     }
 
-    protected function canMove(File $file, User $loggedIn): bool
+    protected function canMove(FileFolder $file, User $loggedIn): bool
     {
         return $file->isBrowser();
     }
 
-    protected function canDelete(File $file, User $loggedIn): bool
+    protected function canDelete(FileFolder $file, User $loggedIn): bool
     {
         return $file->isBrowser();
     }
