de1d36a4cbabb417e5f130a2253a046936d0d2fb
Justin Hoffman <justin@ohmedia.ca>
2023-08-17 10:26:37 -0600

    folder create/edit forms

---
+++ src/Form/Type/FileFolderCreateType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OHMedia\FileBundle\Form\Type;
+
+use OHMedia\FileBundle\Entity\FileFolder;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FileFolderCreateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('locked', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Require login to view this folder and all files within',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => FileFolder::class,
+        ]);
+    }
+}
