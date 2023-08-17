e173c45bc7ed6312352db046141a9e6b285581f0
Justin Hoffman <justin@ohmedia.ca>
2023-08-17 10:02:12 -0600

    get rid of concept of folder move

--- src/Security/Voter/FileFolderVoter.php
+++ src/Security/Voter/FileFolderVoter.php
@@ -13,7 +13,6 @@ class FileFolderVoter extends AbstractEntityVoter
     public const EDIT = 'edit';
     public const LOCK = 'lock';
     public const UNLOCK = 'unlock';
-    public const MOVE = 'move';
     public const DELETE = 'delete';
 
     protected function getAttributes(): array
@@ -24,7 +23,6 @@ class FileFolderVoter extends AbstractEntityVoter
             self::EDIT,
             self::LOCK,
             self::UNLOCK,
-            self::MOVE,
             self::DELETE,
         ];
     }
@@ -67,11 +65,6 @@ class FileFolderVoter extends AbstractEntityVoter
         }
 
         return $folder->isLocked();
-    }
-
-    protected function canMove(FileFolder $folder, User $loggedIn): bool
-    {
-        return $folder->isBrowser();
     }
 
     protected function canDelete(FileFolder $folder, User $loggedIn): bool
