d16e496352e0a13c29979dcc8fe181a83c893da7
Justin Hoffman <justin@ohmedia.ca>
2023-08-18 11:57:33 -0600

    create the wysiwyg functions

---
+++ src/Twig/WysiwygExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace OHMedia\FileBundle\Twig;
+
+use OHMedia\FileBundle\Repository\FileRepository;
+use OHMedia\FileBundle\Repository\ImageRepository;
+use OHMedia\FileBundle\Service\FileManager;
+use OHMedia\FileBundle\Service\ImageManager;
+use OHMedia\WysiwygBundle\Twig\Extension\AbstractWysiwygExtension;
+use Twig\TwigFunction;
+
+class WysiwygExtension extends AbstractWysiwygExtension
+{
+    private $fileManager;
+    private $fileRepository;
+    private $imageManager;
+    private $imageRepository;
+
+    public function __construct(
+        FileManager $fileManager,
+        FileRepository $fileRepository,
+        ImageManager $imageManager,
+        ImageRepository $imageRepository
+    ) {
+        $this->fileManager = $fileManager;
+        $this->fileRepository = $fileRepository;
+        $this->imageManager = $imageManager;
+        $this->imageRepository = $imageRepository;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('file_href', [$this, 'fileHref']),
+            new TwigFunction('image', [$this, 'image']),
+        ];
+    }
+
+    public function fileHref(int $id)
+    {
+        $file = $this->fileRepository->findOneBy([
+            'id' => $id,
+            'browser' => true,
+        ]);
+
+        return $file ? $this->fileManager->getWebPath($file) : '';
+    }
+
+    public function image(int $id, int $width = null)
+    {
+        $image = $this->imageRepository->findOneBy([
+            'id' => $id,
+            'browser' => true,
+        ]);
+
+        if (!$image) {
+            return '';
+        }
+
+        $attributes = [];
+
+        if (null !== $width) {
+            $attributes['width'] = $width;
+        }
+
+        return $this->imageManager->render($image, $attributes);
+    }
+}
