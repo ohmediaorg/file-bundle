b0100aea7e17ea9cabe1ee56a6eb479bfd05be6a
Justin Hoffman <justin@ohmedia.ca>
2023-08-18 09:40:47 -0600

    move code into function

--- src/EventListener/FileSubscriber.php
+++ src/EventListener/FileSubscriber.php
@@ -35,32 +35,37 @@ class FileSubscriber implements EventSubscriber
         $object = $args->getObject();
 
         if ($object instanceof File) {
-            if ($object->isCloned()) {
-                $copy = $this->fileManager->copy($object);
+            $this->prePersistFile($object);
+        }
+    }
 
-                if (!$copy) {
-                    // EDGE CASE: setFile(null) was called then the object was cloned
-                    // everything will be null except for $oldPath
-                    // we want to set $oldPath to null so the file this was cloned
-                    // from is not deleted
-                    $object->setOldPath(null);
+    private function prePersistFile(File $file)
+    {
+        if ($file->isCloned()) {
+            $copy = $this->fileManager->copy($file);
 
-                    return;
-                }
+            if (!$copy) {
+                // EDGE CASE: setFile(null) was called then the file was cloned
+                // everything will be null except for $oldPath
+                // we want to set $oldPath to null so the file this was cloned
+                // from is not deleted
+                $file->setOldPath(null);
 
-                $this->preSaveFile($copy);
-
-                // transfer important values back to $object
-                $object
-                    // IMPORTANT: resetting path prior to calling setFile()
-                    ->setPath(null)
-                    ->setFile($copy->getFile())
-                    ->setPath($copy->getPath())
-                    ->setToken($copy->getToken())
-                ;
-            } else {
-                $this->preSaveFile($object);
+                return;
             }
+
+            $this->preSaveFile($copy);
+
+            // transfer important values back to $file
+            $file
+                // IMPORTANT: resetting path prior to calling setFile()
+                ->setPath(null)
+                ->setFile($copy->getFile())
+                ->setPath($copy->getPath())
+                ->setToken($copy->getToken())
+            ;
+        } else {
+            $this->preSaveFile($file);
         }
     }
 
