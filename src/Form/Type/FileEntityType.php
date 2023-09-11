0434446ad896d61e6a0623cf20c4defb9e85d246
Justin Hoffman <justin@ohmedia.ca>
2023-09-11 14:48:14 -0600

    more fixes

--- src/Form/Type/FileEntityType.php
+++ src/Form/Type/FileEntityType.php
@@ -8,6 +8,7 @@ use OHMedia\FileBundle\Util\MimeTypeUtil;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
