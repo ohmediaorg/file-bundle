4a5ee6fd15807a68a0e0b72467c3c87ed6a36a10
justin-oh <justin@ohmedia.ca>
2024-03-26 14:42:59 -0600

    Create FileFolderMoveType.php

---
+++ src/Form/Type/FileFolderMoveType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OHMedia\FileBundle\Form\Type;
+
+use Doctrine\ORM\EntityRepository;
+use OHMedia\FileBundle\Entity\FileFolder;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FileFolderMoveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $folder = $options['data'];
+
+        $builder
+            ->add('folder', EntityType::class, [
+                'class' => FileFolder::class,
+                'label' => 'Destination Folder',
+                'required' => false,
+                'query_builder' => function (EntityRepository $er) use ($folder) {
+                    return $er->createQueryBuilder('ff')
+                        ->where('ff.browser = 1')
+                        ->andWhere('ff.id <> :id')
+                        ->setParameter('id', $folder->getId())
+                        ->orderBy('LOWER(ff.name)', 'ASC');
+                },
+                'placeholder' => '/',
+                'choice_label' => function (FileFolder $folder) {
+                    return '/'.$folder->getPath();
+                },
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => FileFolder::class,
+        ]);
+    }
+}
